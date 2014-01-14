afd051b42403d93fd60fcae153053f1a111d2927
Ross Martin <rmartin311@gmail.com>
2014-01-14 08:45:57 -0600

    Update DropboxView.js

--- sample/platforms/android/assets/www/js/views/DropboxView.js
+++ sample/platforms/android/assets/www/js/views/DropboxView.js
@@ -1,13 +1,13 @@
 var DropboxView = function (template, listTemplate) {
 
     this.initialize = function () {
-    	
-    	if (! template) return; // making instance just for listFolder method
-    	
-    	var _me = this;
-    	
-    	this.listTemplate = listTemplate;
-    	
+        
+        if (! template) return; // making instance just for listFolder method
+        
+        var _me = this;
+        
+        this.listTemplate = listTemplate;
+        
         this.el = $('<div/>');
 
         this.el.on('click', '#btn-uploadFiles', function(event) {
@@ -33,14 +33,14 @@ var DropboxView = function (template, listTemplate) {
             $('#filePath').html(filePath);
             app.showLoader();
             if( (/\.(gif|jpg|jpeg|tiff|png)$/i).test(filePath) ) {
-            	$('#text, #image').hide();
+                $('#text, #image').hide();
                 dropbox.readData(filePath).done(function(result) {
                     var bytes = new Uint8Array(result);
                     $('#image').attr('src', "data:image/jpeg;base64," + encode(bytes)).show();
                     app.hideLoader();
                 });
             } else {
-            	$('#image, #text').hide();
+                $('#image, #text').hide();
                 dropbox.readString(filePath).done(function(result) {
                     $('#text').html(result).show();
                     app.hideLoader();
@@ -50,8 +50,8 @@ var DropboxView = function (template, listTemplate) {
         });
         
         this.el.on('click', '#btn-back', function(event) {
-        	($('#path').text() == '/') ? app.showExitConfirm() : window.history.back();
-        	event.preventDefault();
+            ($('#path').text() == '/') ? app.showExitConfirm() : window.history.back();
+            event.preventDefault();
         });
         
         $(window).off('haschange').on('hashchange', function() {
@@ -63,22 +63,22 @@ var DropboxView = function (template, listTemplate) {
         $(window).off('orientationchange').on('orientationchange', function(event) {
             setTimeout(function() {
                 var h = $('#content').height(),
-                	w = $('#content').width();
+                    w = $('#content').width();
                 $('#image').css({'max-width':w, 'max-height':h});
                 $('#text').css('max-width', w);
             }, 300);
 
             switch(window.orientation) {
-	            case -90:
-	            case 90:
-	                // landscape
-	                app.loadIcon.css('left', '90px');
-	                break;
-	            default:
-	                //portrait
-	                app.loadIcon.css('left', '60px');
-	                break; 
-	        }
+                case -90:
+                case 90:
+                    // landscape
+                    app.loadIcon.css('left', '90px');
+                    break;
+                default:
+                    //portrait
+                    app.loadIcon.css('left', '60px');
+                    break; 
+            }
         });
          
     }; // end initialize
@@ -104,9 +104,9 @@ DropboxView.prototype.listFolder = function() {
         l = files.length;
         (l > 0) ? $("#noFiles").hide() : $("#noFiles").show();
         for (i = 0; i < l; i++) {
-        	file = files[i];
-        	file.fileName = files[i].path.substr(file.path.lastIndexOf("/") + 1);
-        	file.encodedPath = encodeURIComponent(files[i].path);
+            file = files[i];
+            file.fileName = files[i].path.substr(file.path.lastIndexOf("/") + 1);
+            file.encodedPath = encodeURIComponent(files[i].path);
             if (file.isFolder) {
                 folderArray.push(file);
             } else {
