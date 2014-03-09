76f1e4ceeab9a48bbd991439c5a8c28e5dfc4764
Ross Martin <rmartin311@gmail.com>
2014-03-08 21:32:47 -0600

    Added openFile method to plugin

--- plugin/com/rossmartin/dropbox/DropboxPlugin.java
+++ plugin/com/rossmartin/dropbox/DropboxPlugin.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -15,6 +16,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -38,7 +40,6 @@ import com.dropbox.sync.android.DbxSyncStatus;
 public class DropboxPlugin extends CordovaPlugin {
     
     private static final String TAG = "DropboxPlugin";
-    private static final String PLUGIN_ERROR = "DropboxPlugin Error";
     private static final String APP_KEY = "81v5tm7jg21zk8c"; // Your app key here
     private static final String APP_SECRET = "f9cwicck72tuhpx"; // Your app secret here
     static final int REQUEST_LINK_TO_DBX = 1337;  // This value is up to you, it must be the same as in your main activity though
@@ -98,6 +99,10 @@ public class DropboxPlugin extends CordovaPlugin {
             String dropboxPath = args.getString(0);
             createFolder(dropboxPath, callbackContext);
             return true;
+        } else if (action.equals("openFile")) {
+            String path = args.getString(0);
+            openFile(path, callbackContext);
+            return true;
         }
         return false;
     }
@@ -143,7 +148,7 @@ public class DropboxPlugin extends CordovaPlugin {
                     callbackContext.success(jsonArray);
                 } catch (Exception e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -182,7 +187,7 @@ public class DropboxPlugin extends CordovaPlugin {
                     });
                 } catch (Unauthorized e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -213,14 +218,14 @@ public class DropboxPlugin extends CordovaPlugin {
                         buf.close();
                     } catch (IOException e) {
                         e.printStackTrace();
-                        callbackContext.error(PLUGIN_ERROR);
+                        callbackContext.error(e.getMessage());
                     } finally {
                         file.close();
                     }
                     
                 } catch (Exception e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -243,13 +248,13 @@ public class DropboxPlugin extends CordovaPlugin {
                         callbackContext.success(contents);
                     } catch (IOException e) {
                         e.printStackTrace();
-                        callbackContext.error(PLUGIN_ERROR);
+                        callbackContext.error(e.getMessage());
                     } finally {
                         file.close();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -278,7 +283,7 @@ public class DropboxPlugin extends CordovaPlugin {
                     callbackContext.success();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -333,7 +338,7 @@ public class DropboxPlugin extends CordovaPlugin {
                     callbackContext.success();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -352,7 +357,7 @@ public class DropboxPlugin extends CordovaPlugin {
                     
                     callbackContext.success();
                 } catch (Exception e) {
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -371,7 +376,56 @@ public class DropboxPlugin extends CordovaPlugin {
                     
                     callbackContext.success();
                 } catch (Exception e) {
-                    callbackContext.error(PLUGIN_ERROR);
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
+    }
+    
+    private void openFile(final String path, final CallbackContext callbackContext) {
+        Log.v(TAG, "openFile method executing");
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    DbxFileSystem dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+                    DbxPath filePath = new DbxPath(path);
+                    DbxFile file = dbxFs.open(filePath);
+                    
+                    int fileNameNeedle = path.lastIndexOf("/");
+                    String fileName = path.substring(fileNameNeedle);
+                    Log.v(TAG, "fileName: " + fileName);
+                    
+                    File cacheFolder = cordova.getActivity().getApplicationContext().getExternalCacheDir();
+                    
+                    File tempFile = new File(cacheFolder + "/" + fileName);
+                    Log.v(TAG, "tempFile.getPath(): " + tempFile.getPath());
+                    
+                    FileInputStream inputStream = null;
+                    FileOutputStream ouputStream = new FileOutputStream(tempFile);
+                    // write the contents of the chosen Dropbox file to the tempFile
+                    try {
+                        inputStream = file.getReadStream();
+                        
+                        int read = 0;
+                        byte[] bytes = new byte[1024];
+                 
+                        while ((read = inputStream.read(bytes)) != -1) {
+                            ouputStream.write(bytes, 0, read);
+                        }
+                        
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        callbackContext.error(e.getMessage());
+                    } finally {
+                        inputStream.close();
+                        ouputStream.close();
+                        file.close();
+                    }
+                    
+                    openTempFile(tempFile.getPath());
+                    callbackContext.success();
+                } catch (Exception e) {
+                    callbackContext.error(e.getMessage());
                 }
             }
         });
@@ -433,6 +487,71 @@ public class DropboxPlugin extends CordovaPlugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    private void openTempFile(String url) throws IOException {
+        // Create URI
+        Uri uri = Uri.parse(url);
+
+        Intent intent = null;
+        // Check what kind of file you are trying to open, by comparing the url with extensions.
+        // When the if condition is matched, plugin sets the correct intent (mime) type, 
+        // so Android knew what application to use to open the file
+        
+        if (url.contains(".doc") || url.contains(".docx")) {
+            // Word document
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/msword");
+        } else if(url.contains(".pdf")) {
+            // PDF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/pdf");
+        } else if(url.contains(".ppt") || url.contains(".pptx")) {
+            // Powerpoint file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        } else if(url.contains(".xls") || url.contains(".xlsx")) {
+            // Excel file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.ms-excel");
+        } else if(url.contains(".rtf")) {
+            // RTF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/rtf");
+        } else if(url.contains(".wav")) {
+            // WAV audio file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "audio/x-wav");
+        } else if(url.contains(".gif")) {
+            // GIF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/gif");
+        } else if(url.contains(".jpg") || url.contains(".jpeg")) {
+            // JPG file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/jpeg");
+        } else if(url.contains(".txt")) {
+            // Text file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "text/plain");
+        } else if(url.contains(".mpg") || url.contains(".mpeg") || url.contains(".mpe") || url.contains(".mp4") || url.contains(".avi")) {
+            // Video files
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "video/*");
+        }         
+                
+        //if you want you can also define the intent type for any other file
+        
+        //additionally use else clause below, to manage other unknown extensions
+        //in this case, Android will show all applications installed on the device
+        //so you can choose which application to use
+        
+        else {
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "*/*");
+        }
+
+        this.cordova.getActivity().startActivity(intent);
     }
    
 }
